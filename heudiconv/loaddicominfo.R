f09ba04dfa667bb28403261c0ed424677f923734
peaceisbetter <jackpmanningiii@gmail.com>
2024-01-06 14:09:59 -0600

    Update loaddicominfo.R

--- heudiconv/loaddicominfo.R
+++ heudiconv/loaddicominfo.R
@@ -1,1 +1,1 @@
-data <- read.delim('C:\\Users\\jackpmanning\\OneDrive - Texas A&M University\\Documents\\Projects\\ResiliencyR3\\Codes\\MRI\\heudiconv\\dicominfo.tsv', sep="\t")
+data <- read.delim('C:\\path\\to\\your\file\\dicominfo.tsv', sep="\t")
